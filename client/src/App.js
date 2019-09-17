010cfb5df5bec53cf219c3d62b16439713c769b7
adeoye adela <adeoye.adela@gmail.com>
2019-09-17 23:34:18 +0100

    Create the Movie card Component

--- client/src/App.js
+++ client/src/App.js
@@ -10,14 +10,21 @@ const App = () => {
   const [savedList, setSavedList] = useState( [] );
 
   const addToSavedList = movie => {
-    setSavedList( [...savedList, movie] );
-  };
+
+//     savedList.forEach((item)=>{
+// if(!item.includes(item.id)){
+  setSavedList( [...savedList, movie] );
+}
+// console.log(savedList)
+
+//     })
+//   };
 
   return (
     <div>
       <SavedList list={savedList} />
       <Route exact path="/" component={MovieList}/>
-      <Route exact path="/movies/:id" component={Movie}/>
+      <Route exact path="/movies/:id" render={(props) => <Movie {...props} addToSavedList={addToSavedList} />}/>
     </div>
   );
 };
